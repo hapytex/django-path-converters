9f58994ca163cb00b929ffe179c425ca23aa5324
Willem Van Onsem <vanonsem.willem@gmail.com>
2023-01-22 13:31:43 +0100

    check examples, accepts and items

--- django_path_converters/converters.py
+++ django_path_converters/converters.py
@@ -1,24 +1,47 @@
-from datetime import date
+from datetime import date, datetime
 import re
 from django.apps import apps
+from django.core.exceptions import AppRegistryNotReady
 from django.db.models import Model
 from django.shortcuts import get_object_or_404
 from django.urls import register_converter
 
 
 class PathConverter(type):
+    registered = []
+    check_regex = True
+    check_examples = True
+
     def __new__(cls, name, bases, attrs):
-        if 'regex' in attrs:
+        if cls.check_regex and 'regex' in attrs:
             # validate regex
             re.compile(attrs['regex'])
-        examples = attrs['examples']
+        examples = attrs.get('examples')
         # wrap in a tuple in case of a single example
         if isinstance(examples, str):
-            attrs['examples'] = (examples,)
-        klass = super().__new__(name, bases, attrs)
-        if 'name' in attrs:
-            register_converter(klass, attrs[name])
+            examples = attrs['examples'] = (examples,)
+        klass = super().__new__(cls, name, bases, attrs)
+        name = attrs.get('name')
+        if name:
+            register_converter(klass, name)
+            cls.registered.append(klass)
+        instance = klass()
+        if cls.check_examples and examples:
+            for example in examples:
+                try:
+                    instance.to_python(example)
+                except (AppRegistryNotReady,):
+                    pass
         return klass
+
+    def data_dict(cls):
+        return {
+            'name': f'`{cls.name}`',
+            'regex': f'`{cls.regex}`',
+            'examples': '<br>'.join(f'`{ex}`' for ex in cls.examples),
+            'type': cls.accepts[0].__qualname__,
+            'accepts': '<br>'.join(k.__qualname__ for k in cls.accepts),
+        }
 
 
 class BaseConverter(metaclass=PathConverter):
@@ -43,8 +66,8 @@ class DateConverter(BaseConverter):
     accepts = (date,)
     examples = '2023-01-21'
 
-    def to_python(self, value):
-        return datetime.strptime(value, self.date_format).date()
+    def to_python(self, value, date_format=None):
+        return datetime.strptime(value, date_format or self.date_format).date()
 
     def inner_to_url(self, value):
         return value.strftime(self.date_format)
@@ -59,12 +82,18 @@ class MonthConverter(DateConverter):
 
 class WeekConverter(DateConverter):
     name = 'week'
-    date_format = '%Y-W%V'
+    date_format = '%G-W%V'
     regex = '[0-9]{4}[-]W(?:0?[1-9]|[1-4][0-9]|5[0-3])'
     examples = '2023-W03'
+    week_format = '%u'
+    week_day = '1'
+
+    def to_python(self, value):
+        return super().to_python(f'{value}{self.week_day}', f'{self.date_format}{self.week_format}')
 
 
-class DateRangeConverter(BaseConverter):
+
+class DateRangeConverter(DateConverter):
     name = 'date_range'
     regex = '/'.join((DateConverter.regex,)*2)
     examples = '2023-01-21/2023-03-25'
