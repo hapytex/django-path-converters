8513643c8bdb2415fc5c5c2e672c20efb46c0866
Willem Van Onsem <willem.vanonsem@prosafco.be>
2024-03-06 12:57:45 +0100

    use interegular instead

--- django_path_converters/management/commands/url_overlap.py
+++ django_path_converters/management/commands/url_overlap.py
@@ -3,14 +3,14 @@ from itertools import islice
 from django.core.management.base import BaseCommand
 from django.urls import get_resolver
 from greenery import parse as gparse
+from interegular import parse_pattern
 
-from django_path_converters.utils import strip_capture_groups
 from xeger import Xeger
 from random import randint
 import sys
 import re
 from argparse import BooleanOptionalAction
-
+from django_path_converters.utils import strip_capture_groups
 
 class Command(BaseCommand):
     help = "Search if two or more URLs overlap. The exit code shows the number of overlaps."
@@ -28,7 +28,7 @@ class Command(BaseCommand):
             subregex = subregex[:-2]
         regex = f'{prefix}{subregex}'
         if resolver.callback is not None:
-            yield regex, [gparse(strip_capture_groups(regex)), 0]
+            yield regex, [parse_pattern(strip_capture_groups(regex)).to_fsm().reduce(), 0]
         for subresolver in getattr(resolver, 'url_patterns', ()):
             yield from self.produce_regexes(subresolver, regex)
 
@@ -39,30 +39,31 @@ class Command(BaseCommand):
             return True
 
     def explain_failure(self, xeger, regex1, regex2, full1, full2):
-        intersect = str(regex1 & regex2)
-        fail = str(intersect) != '[]'
-        reorder = False
+        intersect = (regex1 & regex2).reduce()
+        fail = not intersect.empty()
         if fail:
+            full_overlap = 'fully ' if (regex1 ^ regex2).empty() else ''
+            reorder = (regex2 - regex1).empty()
             with_example = False
             example = None
             try:
-                example = xeger.xeger(intersect)
+                overlap = str(gparse(strip_capture_groups(full1)) & gparse(strip_capture_groups(full2)))
+                example = xeger.xeger(overlap)
                 capture1 = re.match(full1, example)
                 capture2 = re.match(full2, example)
                 with_example = capture1 and capture2
             except:
                 pass
-            sys.stderr.write(f'[\x1b[31m✗\x1b[0m] patterns \x1b[34m{full1}\x1b[0m and \x1b[34m{full2}\x1b[0m overlap\n')
+            sys.stderr.write(f'[\x1b[31m✗\x1b[0m] patterns \x1b[34m{full1}\x1b[0m and \x1b[34m{full2}\x1b[0m {full_overlap}overlap\n')
             if with_example:
                 sys.stderr.write(f'      for example with \x1b[35m{example!r}\x1b[0m\n')
                 groupdict1 = capture1.groupdict()
                 groupdict2 = capture2.groupdict()
                 self.explain_capture(groupdict1, _hasnext=groupdict2)
                 self.explain_capture(groupdict2, _type='second')
-                if str(regex2.difference(regex1)) == '[]':
-                    sys.stderr.write(f'    \x1b[33;40m!\x1b[0m since all captures by the second pattern are also captured by the first pattern,\n')
-                    sys.stderr.write(f'      the second pattern will never fire, you therefore probably should reorder the patterns.\n')
-                    reorder = True
+            if reorder:
+                sys.stderr.write(f'    \x1b[33;40m!\x1b[0m since all captures by the second pattern are also captured by the first pattern,\n')
+                sys.stderr.write(f'      the second pattern will never fire, you therefore probably should reorder the patterns.\n')
             sys.stderr.write(f'\n')
         return fail, reorder
 
@@ -73,7 +74,7 @@ class Command(BaseCommand):
         fail = 0
         if seed is None:
             seed = randint(0, 2**64)
-        xeger = Xeger(limit=5, seed=seed)  # shorter URLs, make the problem clearer
+        xeger = Xeger(limit=1, seed=seed)  # shorter URLs, make the problem clearer
 
         nooverlaps = []
         for i, (full1, [regex1, subfail]) in enumerate(regexes, 1):
@@ -97,4 +98,4 @@ class Command(BaseCommand):
         status = fail // 2
         if status:
             status = ((status - 1) % 253) + 1
-        exit(fail // 2)  # we count each failure twice
+        exit(fail // 2)  # we count each failure twi
